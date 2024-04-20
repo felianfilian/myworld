253121878a1a694bb10713f6a2448e0eda292127
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-04-20 08:23:02 +0200

    create testing page

--- shop_list/members/views.py
+++ shop_list/members/views.py
@@ -21,3 +21,6 @@ def details(request, id):
 def main(request):
   template = loader.get_template("main.html")
   return HttpResponse(template.render())
+
+def testing(request):
+  template = loader.get_template('template.html')
