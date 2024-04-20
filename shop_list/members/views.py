d600aecc4726348788be59acb996fb3d6388efe9
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-04-20 08:25:57 +0200

    create test page

--- shop_list/members/views.py
+++ shop_list/members/views.py
@@ -24,3 +24,7 @@ def main(request):
 
 def testing(request):
   template = loader.get_template('template.html')
+  context = {
+    'fruits': ['Apple', 'Banana', 'Cherry'],
+  }
+  return HttpResponse(template.render(context, request))
