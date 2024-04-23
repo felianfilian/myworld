62569feb336dcab276462e2cf2982e6b8db8c39f
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-04-23 03:00:07 +0200

    field lookups

--- shop_list/members/views.py
+++ shop_list/members/views.py
@@ -24,7 +24,7 @@ def main(request):
 
 def testing(request):
   # mymembers = Member.objects.all().values()
-  mymembers = Member.objects.filter(firstname='mario').values();
+  mymembers = Member.objects.filter(firstname__startswith='p').values();
   template = loader.get_template('template.html')
   context = {
     'fruits': ['Apple', 'Banana', 'Cherry'],
